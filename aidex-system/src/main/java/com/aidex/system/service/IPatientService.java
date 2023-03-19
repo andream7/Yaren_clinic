bab4581722b012777135b3bfcf77adf120d73ba6
andream7 <2236407598@qq.com>
2023-03-19 16:01:36 +0800

    feat: add PIPatientService

--- aidex-system/src/main/java/com/aidex/system/service/IPatientService.java
+++ aidex-system/src/main/java/com/aidex/system/service/IPatientService.java
@@ -1,142 +1,79 @@
 package com.aidex.system.service;
 
-import com.aidex.common.core.domain.entity.Patient;
-import com.aidex.common.core.service.BaseService;
-import com.alibaba.fastjson2.JSONObject;
-import com.github.pagehelper.PageInfo;
-import org.springframework.transaction.annotation.Transactional;
-
+import com.aidex.system.domain.vo.PatientBasicInfo;
 import java.util.List;
-import java.util.Map;
 
 /**
- * 用户 业务层
+ * 普通用户基础信息接口
  *
- * @author ruoyi
  */
-public interface IPatientService extends BaseService<Patient>
-{
 
-    @Override
-    public PageInfo<Patient> findPage(Patient patient);
+public interface IPatientService {
+
+//    /**
+//     * 发送注册短信
+//     *
+//     * @param phone 手机号码
+//     * @return 是否成功
+//     */
+//    boolean sendMessage(String phone);
+//
+//    /**
+//     * 校验短信验证码
+//     *
+//     * @param phone 手机号
+//     * @param code  验证码
+//     * @return 是否正确
+//     */
+//    boolean verifyCode(String phone, String code);
+//
+//    /**
+//     * 创建普通用户信息
+//     *
+//     * @param param 普通用户信息参数
+//     * @return 是否成功
+//     */
+//    boolean insert(UserRegisterParam param);
 
     /**
-     * 校验用户是否允许操作
+     * 更新普通用户信息
      *
-     * @param user 用户信息
+     * @param id    用户编号
+     * @param param 普通用户信息参数
+     * @return 是否成功
      */
-    public void checkUserAllowed(Patient user);
+    boolean update(Long id, PatientBasicInfo param);
 
     /**
-     * 新增用户信息
+     * 删除普通用户信息
      *
-     * @param user 用户信息
-     * @return 结果
+     * @param id 用户编号
+     * @return 是否成功
      */
-    @Transactional(readOnly = false)
-    public boolean insertUser(Patient user);
+    boolean delete(Long id);
 
     /**
-     * 修改用户信息
+     * 获取普通用户信息
      *
-     * @param user 用户信息
-     * @return 结果
+     * @param id 用户编号
+     * @return 普通用户信息
      */
-    @Transactional(readOnly = false)
-    public boolean updateUser(Patient user);
+    PatientBasicInfo getById(Long id);
 
     /**
-     * 修改用户状态
+     * 通过关键词，查找用户信息列表
      *
-     * @param user 用户信息
-     * @return 结果
+     * @param pageNum  第几页
+     * @param pageSize 页大小
+     * @return 用户信息列表
      */
-    @Transactional(readOnly = false)
-    public boolean updateUserStatus(Patient user);
+    List<PatientBasicInfo> list(Integer pageNum, Integer pageSize);
 
     /**
-     * 修改用户基本信息
+     * 判断用户信息是否存在
      *
-     * @param user 用户信息
-     * @return 结果
+     * @param id 用户编号
+     * @return 是否存在
      */
-    @Transactional(readOnly = false)
-    public boolean updateUserProfile(Patient user);
-
-
-    /**
-     * 记录登录信息
-     *
-     * @param user 用户信息
-     * @return 结果
-     */
-    @Transactional(readOnly = false)
-    public boolean updateUserLoginInfo(Patient user);
-
-    /**
-     * 修改用户头像
-     *
-     * @param userName 用户名
-     * @param avatar 头像地址
-     * @return 结果
-     */
-    @Transactional(readOnly = false)
-    public boolean updateUserAvatar(String userName, String avatar);
-
-    /**
-     * 重置用户密码
-     *
-     * @param userName 用户名
-     * @param password 密码
-     * @return 结果
-     */
-    public int resetUserPwd(String userName, String password);
-
-    /**
-     * 通过用户ID删除用户
-     *
-     * @param userId 用户ID
-     * @return 结果
-     */
-    @Transactional(readOnly = false)
-    public boolean deleteUserById(String userId);
-
-    /**
-     * 批量删除用户信息
-     *
-     * @param userIds 需要删除的用户ID
-     * @return 结果
-     */
-    @Transactional(readOnly = false)
-    public int deleteUserByIds(String[] userIds);
-
-
-    public List<Map<String,Object>> getUserInfoByIds(JSONObject userIdsObj);
-
-    /**
-     * 注册用户信息
-     *
-     * @param user 用户信息
-     * @return 结果
-     */
-    public boolean registerUser(Patient user);
-
-    /**
-     * 刷新缓存
-     * @return 结果
-     */
-    public void refreshCache();
-
-    /**
-     * 加载参数缓存数据
-     */
-    public void loadingUserCache();
-
-    /**
-     * 校验用户是否有数据权限
-     *
-     * @param userId 用户id
-     */
-    public void checkUserDataScope(String userId);
-
+    boolean count(Long id);
 }
