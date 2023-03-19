6b939c73fbde290cb50f564e69a0a810fe62de13
andream7 <2236407598@qq.com>
2023-03-19 21:09:12 +0800

    feat: VisitPlanParam

---
+++ aidex-system/src/main/java/com/aidex/system/dto/param/VisitPlanParam.java
@@ -0,0 +1,58 @@
+package com.aidex.system.dto.param;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author YuJian95  clj9509@163.com
+ * @date 2020/3/1
+ */
+
+@ApiModel(value = "VisitPlanParam", description = "出诊计划参数")
+@Data
+public class VisitPlanParam {
+
+    /**
+     * 专科编号
+     *
+     * @mbg.generated
+     */
+    @ApiModelProperty(value = "专科编号")
+    private String specialId;
+
+    /**
+     * 门诊编号
+     *
+     * @mbg.generated
+     */
+    @ApiModelProperty(value = "门诊编号")
+    private Long outpatientId;
+
+    /**
+     * 医生编号
+     *
+     * @mbg.generated
+     */
+    @ApiModelProperty(value = "医生编号")
+    private Long doctorId;
+
+    /**
+     * 时间段 1：上午，2：下午
+     *
+     * @mbg.generated
+     */
+    @ApiModelProperty(value = "时间段 1：上午，2：下午")
+    private Integer time;
+
+    /**
+     * 出诊日期
+     *
+     * @mbg.generated
+     */
+    @ApiModelProperty(value = "出诊日期")
+    private Date day;
+
+}
