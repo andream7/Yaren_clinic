ff5e44b2a1e5502eed0a31a6cfa1f40346394c3f
andream7 <2236407598@qq.com>
2023-03-21 21:11:25 +0800

    feat: modify UserCaseController

--- aidex-controller/src/main/java/com/aidex/web/controller/hospital/UserCaseController.java
+++ aidex-controller/src/main/java/com/aidex/web/controller/hospital/UserCaseController.java
@@ -7,6 +7,7 @@ import com.aidex.system.dto.param.UserCaseUpdateParam;
 import com.aidex.system.entity.UserCase;
 import com.aidex.system.service.ISysUserService;
 import com.aidex.system.service.IUserCaseService;
+import com.aidex.system.service.IUserMedicalCardService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -15,21 +16,33 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-@Api(value = "患者模块", tags = "患者病例接口")
+/**
+ * @author YuJian95  clj9509@163.com
+ * @date 2020/3/2
+ */
+
+@Api(value = "用户模块", tags = "用户病例接口")
 @RestController
 @CrossOrigin
-@RequestMapping("/case")
-public class CaseController {
+@RequestMapping("/user")
+public class UserCaseController {
 
     @Resource
     private IUserCaseService caseService;
 
     @Resource
+    private IUserMedicalCardService cardService;
+
+    @Resource
     private ISysUserService userService;
 
-    @ApiOperation(value = "添加用户病例", notes = "传入 用户病例参数（患者编号、预约编号、医生编号、病例详情）")
-    @PostMapping(value = "/")
+    @ApiOperation(value = "添加用户病例", notes = "传入 用户病例参数（就诊卡编号、预约编号、医生编号、病例详情）")
+    @RequestMapping(value = "/case", method = RequestMethod.POST)
     public CommonResult insertUserCase(@RequestBody UserCaseParam param) {
+
+        if (!cardService.countCardId(param.getCardId())) {
+            return CommonResult.validateFailed("不存在，该就诊卡编号！");
+        }
 
         if (!userService.count(param.getDoctorId())) {
             return CommonResult.validateFailed("不存在，该医生编号！");
@@ -43,8 +56,8 @@ public class CaseController {
     }
 
     @ApiOperation(value = "更新用户病例", notes = "传入 病例编号、用户病例更新参数（病例详情）")
-    @ApiImplicitParam(name = "cardId", value = "病历号", paramType = "path", dataType = "Long", required = true)
-    @PutMapping(value = "/{id}")
+    @ApiImplicitParam(name = "cardId", value = "就诊卡编号", paramType = "path", dataType = "Long", required = true)
+    @RequestMapping(value = "/case/{id}", method = RequestMethod.PUT)
     public CommonResult updateUserCase(@PathVariable Long id, @RequestBody UserCaseUpdateParam param) {
         if (!caseService.count(id)) {
             return CommonResult.validateFailed("不存在，该病例编号！");
@@ -58,8 +71,8 @@ public class CaseController {
     }
 
     @ApiOperation(value = "删除用户病例", notes = "传入 病例编号")
-    @ApiImplicitParam(name = "cardId", value = "病历编号", paramType = "path", dataType = "Long", required = true)
-    @DeleteMapping(value = "/{id}")
+    @ApiImplicitParam(name = "cardId", value = "就诊卡编号", paramType = "path", dataType = "Long", required = true)
+    @RequestMapping(value = "/case/{id}", method = RequestMethod.DELETE)
     public CommonResult deleteUserCase(@PathVariable Long id) {
 
         if (!caseService.count(id)) {
@@ -74,28 +87,32 @@ public class CaseController {
 
     }
 
-    @ApiOperation(value = "获取用户病例", notes = "传入 患者编号、预约编号")
+    @ApiOperation(value = "获取用户病例", notes = "传入 就诊卡编号、预约编号")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "cardId", value = "患者编号", paramType = "query", dataType = "Long",
+            @ApiImplicitParam(name = "cardId", value = "就诊卡编号", paramType = "query", dataType = "Long",
                     required = true),
             @ApiImplicitParam(name = "appointmentId", value = "预约编号", paramType = "query", dataType = "Long",
                     required = true),
     })
-    @GetMapping(value = "/{id}")
-    public CommonResult<UserCase> getUserCase(@RequestParam Long Id, @RequestParam Long appointmentId) {
+    @RequestMapping(value = "/case", method = RequestMethod.GET)
+    public CommonResult<UserCase> getUserCase(@RequestParam Long cardId, @RequestParam Long appointmentId) {
 
-        return CommonResult.success(caseService.get(Id, appointmentId));
+        if (!cardService.countCardId(cardId)) {
+            return CommonResult.validateFailed("不存在，该就诊卡编号！");
+        }
+
+        return CommonResult.success(caseService.get(cardId, appointmentId));
     }
 
-    @ApiOperation(value = "用户病例列表", notes = "传入 患者编号、第几页、页大小")
+    @ApiOperation(value = "查找用户病例", notes = "传入 就诊卡编号、第几页、页大小")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "cardId", value = "患者编号", paramType = "query", dataType = "Long"),
+            @ApiImplicitParam(name = "cardId", value = "就诊卡编号", paramType = "query", dataType = "Long"),
             @ApiImplicitParam(name = "pageNum", value = "第几页", paramType = "query", dataType = "Integer",
                     required = true),
             @ApiImplicitParam(name = "pageSize", value = "页大小", paramType = "query", dataType = "Integer",
                     required = true),
     })
-    @GetMapping(value = "/list")
+    @RequestMapping(value = "/case/list", method = RequestMethod.GET)
     public CommonResult<CommonPage<UserCase>> listUserCase(@RequestParam(required = false) Long cardId,
                                                            @RequestParam Integer pageNum, @RequestParam Integer pageSize) {
 
